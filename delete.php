0e334292f9f04d126420de73e7e7617a576dc9e2
SaidSouli <moneyproject34@gmail.com>
2024-04-08 03:35:54 +0100

    dhia

--- delete.php
+++ delete.php
@@ -1,16 +1,14 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: PUT, GET, POST");
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 // include database connection
 include 'config/database.php';
  
 try {
-     
     // get record ID
-    // isset() is a PHP function used to verify if a value is there or not
-    $id=isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
- header("content-type:text/html; charset=UTF-8");
+    $id = isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
+    header("content-type:text/html; charset=UTF-8");
     // delete query
     $query = "DELETE FROM products WHERE p_id = ?";
     $stmt = $con->prepare($query);
@@ -20,8 +18,8 @@ try {
         // redirect to read records page and 
         // tell the user record was deleted
        echo json_encode(array('result'=>'success'));
-    }else{
-        echo json_encode(array('result'=>'fail'));
+    } else {
+        echo json_encode(array('result'=>'fail', 'message' => 'Failed to delete the product.'));
     }
 }
  
